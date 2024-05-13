5720e818b5db543c78a851383ae5240690b16509
Suyog Patil <suyog0057@gmail.com>
2024-05-13 10:42:14 +0000

    test: fix test for view most recent 10 threads

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -14,9 +14,9 @@ suite('Functional Tests', function () {
     const replyText = 'Testing -> reply text';
     const delete_password = 'password';
 
-    // suiteTeardown(async function () {
-    //     await threadDAO.deleteAllThreadsByBoard(board);
-    // });
+    suiteTeardown(async function () {
+        await threadDAO.deleteAllThreadsByBoard(board);
+    });
 
     test('Creating a new thread: POST request to /api/threads/{board}', async function () {
         const response = await chai.request(server).post(`/api/threads/${board}`).send({
@@ -41,22 +41,29 @@ suite('Functional Tests', function () {
 
     test('Viewing the 10 most recent threads with 3 replies each: GET request to /api/threads/{board}', async function () {
         const response = await chai.request(server).get(`/api/threads/${board}`);
+
         chai.expect(response.statusCode).to.equal(200);
         chai.assert.isArray(response.body);
         chai.expect(response.body).to.have.length.lessThan(11); // must contain only 10
-        chai.expect(response.body[0]).to.not.have.property('reported');
-        chai.expect(response.body[0]).to.not.have.property('delete_password');
-        chai.expect(response.body[0]).to.have.property('_id');
-        chai.expect(response.body[0]).to.have.property('created_on');
-        chai.expect(response.body[0]).to.have.property('bumped_on');
-        chai.expect(response.body[0]).to.have.property('replies');
-        chai.expect(response.body[0]['board']).to.equal(board);
-        chai.expect(response.body[0]['replies']).to.have.length.lessThan(4); // must contain only 3
-        chai.expect(response.body[0]['replies'][0]).to.have.property('text');
-        chai.expect(response.body[0]['replies'][0]).to.have.property('_id');
-        chai.expect(response.body[0]['replies'][0]).to.not.have.property('created_on');
-        chai.expect(response.body[0]['replies'][0]).to.not.have.property('reported');
-        chai.expect(response.body[0]['replies'][0]).to.not.have.property('delete_password');
+        for (let i = 0; i < response.body.length; i++) {
+            const element = response.body[i];
+            chai.expect(element).to.not.have.property('reported');
+            chai.expect(element).to.not.have.property('delete_password');
+            chai.expect(element).to.have.property('_id');
+            chai.expect(element).to.have.property('created_on');
+            chai.expect(element).to.have.property('bumped_on');
+            chai.expect(element).to.have.property('replies');
+            chai.expect(element['board']).to.equal(board);
+            chai.expect(element['replies']).to.have.length.lessThan(4); // must contain only 3
+
+            for (let j = 0; j < element['replies'].length; j++) {
+                chai.expect(element['replies'][j]).to.have.property('_id');
+                chai.expect(element['replies'][j]).to.have.property('text');
+                chai.expect(element['replies'][j]).to.have.property('created_on');
+                chai.expect(element['replies'][j]).to.not.have.property('delete_password');
+                chai.expect(element['replies'][j]).to.not.have.property('reported');
+            }
+        }
     });
 
     test('Creating a new reply: POST request to /api/replies/{board}', async function () {
@@ -111,10 +118,13 @@ suite('Functional Tests', function () {
         chai.assert.isArray(response.body['replies']);
         chai.expect(response.body['board']).to.equal(board);
         chai.expect(Date.parse(response.body['bumped_on'])).to.be.greaterThan(Date.parse(response.body['created_on']));
-        chai.expect(response.body['replies'][0]).to.have.property('_id');
-        chai.expect(response.body['replies'][0]).to.have.property('text');
-        chai.expect(response.body['replies'][0]).to.have.property('created_on');
-        chai.expect(response.body['replies'][0]).to.not.have.property('delete_password');
-        chai.expect(response.body['replies'][0]).to.not.have.property('reported');
+        for (const key in response.body['replies']) {
+            chai.expect(response.body['replies'][key]).to.have.property('_id');
+            chai.expect(response.body['replies'][key]).to.have.property('text');
+            chai.expect(response.body['replies'][key]).to.have.property('created_on');
+            chai.expect(response.body['replies'][key]).to.not.have.property('delete_password');
+            chai.expect(response.body['replies'][key]).to.not.have.property('reported');
+        }
+
     });
 });
